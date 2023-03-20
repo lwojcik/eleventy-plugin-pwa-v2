4c83244741d4bf17dd875335ffd30606f2c13749
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-03-20 20:41:40 +0100

    Increase test length

--- test/builder.test.js
+++ test/builder.test.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const rimraf = require('rimraf');
-const path = require('path');
-const buildr = require('../src/builder');
-const stub = 'test/stub';
+const fs = require("fs");
+const rimraf = require("rimraf");
+const path = require("path");
+const buildr = require("../src/builder");
+const stub = "test/stub";
 
 afterAll(() => {
   const opt = { glob: true };
@@ -13,19 +13,19 @@ afterAll(() => {
   });
 });
 
-it('dir not exist', async () => {
-  const out = await buildr({}, path.join(stub, 'shadow'));
+it("dir not exist", async () => {
+  const out = await buildr({}, path.join(stub, "shadow"));
   await expect(out).toMatch(/^ENOENT/);
 });
 
-it('dir exist but no content to index', async () => {
-  const dir = path.join(stub, 'empty');
+it("dir exist but no content to index", async () => {
+  const dir = path.join(stub, "empty");
   fs.mkdirSync(dir);
   const out = await buildr({}, dir);
   await expect(out).toMatch(/^No/);
 });
 
-it('dir exist and normal', async () => {
+it("dir exist and normal", async () => {
   const out = await buildr({}, stub);
   await expect(out).toMatch(/precached/);
-});
+}, 30000);
